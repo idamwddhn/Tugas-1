cc1f75d2d16b7e1a4a6f4cb94447d2535a60b4ac
idamwddhn <127532576+idamwddhn@users.noreply.github.com>
2023-03-11 15:56:16 +0800

    Asgdll.java

---
+++ Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,23 @@
+/* NIM              : 13020210026
+   Nama             : Ida Mawaddah Nur
+   Hari/Tanggal     : Senin, 06 Maret 2023 
+   Waktu Pengerjaan : 07.00 */
+
+public class Asgdll {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        float f=20.0f;
+        double fll;
+
+        /* Algoritma */
+        fll= 10.0f;
+        System.out.println("f : " +f+ "\nfll: " +fll);
+    }
+    
+}
